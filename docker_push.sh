d8c46ccaa27ed668724bbc5e0d1f9ed86250e68d
Medya Gh <medya@google.com>
2019-08-18 15:14:42 -0700

    travis doesnt support multi deploy steps

--- docker_push.sh
+++ docker_push.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+set -eux -o pipefail
 
-docker push medyagh/kic:develop
+docker push medyagh/kic:v1.15.0
+docker push medyagh/kic:v1.14.3
+docker push medyagh/kic:v1.13.7
+docker push medyagh/kic:v1.12.9
+docker push medyagh/kic:v1.11.10
