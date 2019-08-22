92657f5a88a7d603021e9b82cc754f928c673c79
Medya Gh <medya@google.com>
2019-08-22 16:43:41 -0700

    travis update

--- e2e-test.sh
+++ e2e-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eux -o pipefail
 
-if [[ -z "${CI}"] ]; then
+if [[ ! -z "${INSIDE_TRAVIS}" ]]; then
     curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
     chmod +x ./kubectl
     sudo mv ./kubectl /usr/local/bin/kubectl
