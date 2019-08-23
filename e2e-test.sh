92e7075e0ebd7f9680177500b4ad1afaf75baa57
Medya Gh <medya@google.com>
2019-08-22 17:47:41 -0700

    e2e

--- e2e-test.sh
+++ e2e-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eux -o pipefail
 
- if ! kubectl version &>/dev/null; then
+ if ! kubectl &>/dev/null; then
     echo "WARNING: No kubectl installation found in your enviroment."
     curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
     chmod +x ./kubectl
@@ -20,10 +20,13 @@ lsof -ti tcp:8080 | xargs kill || true
 export KUBECONFIG=$HOME/.kube/kic-config-m5
 sleep 3 
 kubectl wait deployment -l k8s-app=kube-dns --for condition=available --timeout=120s -n kube-system
+kubectl get pods -A
+kubectl wait pod -l component=kube-apiserver --for condition=Initialized --timeout=100s -n kube-system
+kubectl wait pod -l component=kube-apiserver --for condition=ContainersReady --timeout=100s -n kube-system
+kubectl wait pod -l component=kube-apiserver --for condition=PodScheduled --timeout=100s -n kube-system
 kubectl wait pod -l component=kube-apiserver --for condition=Ready --timeout=100s -n kube-system
 kubectl wait pod -l component=etcd --for condition=Ready --timeout=100s -n kube-system
 
-kubectl get pods -A
 kubectl run hello-minikube --image=k8s.gcr.io/echoserver:1.4 --port=8080
 kubectl expose deployment hello-minikube --type=NodePort
 kubectl wait deployment -l run=hello-minikube --for condition=available --timeout=100s
