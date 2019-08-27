fbc250e6cedcef4e4ccfeb900813a7da9be3ff9f
Medya Gh <medya@google.com>
2019-08-27 03:27:42 -0700

    organize files a bit

--- pkg/oci/oci.go
+++ pkg/oci/oci.go
@@ -9,6 +9,7 @@ import (
 	"github.com/cenkalti/backoff"
 	"github.com/medyagh/kic/pkg/config/cri"
 	"github.com/medyagh/kic/pkg/runner"
+	"github.com/pkg/errors"
 )
 
 // can be podman
@@ -165,4 +166,14 @@ func generatePortMappings(portMappings ...cri.PortMapping) []string {
 		result = append(result, publish)
 	}
 	return result
+}
+
+// Save saves an image archive "docker/podman save"
+func Save(image, dest string) error {
+	cmd := runner.Command(DefaultOCI, "save", "-o", dest, image)
+	lines, err := runner.CombinedOutputLines(cmd)
+	if err != nil {
+		return errors.Wrapf(err, "saving image to tar failed, output %s", lines[0])
+	}
+	return nil
 }
