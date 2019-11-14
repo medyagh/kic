b0116188fcaec48569012484c3d6dba800f120f6
Medya Gh <medya@google.com>
2019-11-14 11:36:48 -0800

    update images

--- pkg/image/image.go
+++ pkg/image/image.go
@@ -8,27 +8,27 @@ func NameForVersion(ver string) (string, error) {
 
 	switch ver {
 	case "v1.11.10":
-		return "medyagh/kic:v1.11.10@sha256:6cfec38db507b9b35b8779010640fe7f59d9e27b111cb9079fb42d801116d090", nil
+		return "medyagh/kic:v1.11.10@sha256:dae9633404059f3b1debc0ca016e1a2efcfd3123793264422a873f074beb3ebc", nil
 	case "v1.12.8":
-		return "medyagh/kic:v1.12.8@sha256:fb4827ffa316b421985e6c279048195b6d207feed25d0b0d20d3b0df0d78206c", nil
+		return "medyagh/kic:v1.12.8@sha256:a21a4387d660c6abbc0857174bdec365ec7f6927a54e166818e8adecfa037d6c", nil
 	case "v1.12.9":
-		return "medyagh/kic:v1.12.9@sha256:518265de75580eda358f91789e720c1a162f4ce131ee7b605d26836a06967f20", nil
+		return "medyagh/kic:v1.12.9@sha256:07142d61b988aec8706dc71d34344f31e1cfac92410830a5d34b3e9012a02757", nil
 	case "v1.12.10":
-		return "medyagh/kic:v1.12.10@sha256:f565c866813e4184d3454ff0d576b4dcdeddf21b9b9ee5da3fcd88099967ead9", nil
+		return "medyagh/kic:v1.12.10@sha256:1da46fe83a3bc918288c7ed9133105a8dea95d141a6de018b4b13f1cad6808da", nil
 	case "v1.13.6":
-		return "medyagh/kic:v1.13.6@sha256:0fd2095ccdd145acbc05922485c99703e7378e3242f9fa78197e71368ee464cb", nil
+		return "medyagh/kic:v1.13.6@sha256:294f24dc8bee373d4343cdf29e4223b134dda977d830d6140f6d4db55eda39aa", nil
 	case "v1.13.7":
-		return "medyagh/kic:v1.13.7@sha256:f10a8c4fde2a3849fb5954294457c8cd7a4baae075cbcdf115ca286430acb4eb", nil
+		return "medyagh/kic:v1.13.7@sha256:6012d101f19bd502981591027ea9dfc2e660c039b5b4e5e6ad95a5af41db47a0", nil
 	case "v1.14.3":
-		return "medyagh/kic:v1.14.3@sha256:8f7be744af5776c47247202fc08bd4eaf7173178ce26038b40f234f682d46192", nil
+		return "medyagh/kic:v1.14.3@sha256:533dc16fd161244cc053b88d8091878efae5437b141f51d9b3bf7069c05f4725", nil
 	case "v1.15.0":
-		return "medyagh/kic:v1.15.0@sha256:103f0592f6073d4d4ea1167e7e487d4555a25d140d297fc2d2a3fb2ec3ecd4d7", nil
+		return "medyagh/kic:v1.15.0@sha256:8f9e16c26f65e23ed6637c67be4e4cec287ce679f9c79f3eae1a09d8eeb7c853", nil
 	case "v1.15.3":
-		return "medyagh/kic:v1.15.3@sha256:63358cd2952dc63d37c72b118a745f8080a72f7a8f3928ffa721358eb85cd14b", nil
+		return "medyagh/kic:v1.15.3@sha256:8a3694870e5eb2123e1ad189694702ad64876fa22f5f0b3bea042129bd296f93", nil
 	case "v1.16.1":
-		return "medyagh/kic:v1.16.1@sha256:ca01b2104c94422fdbf55833d6fce2192b9d15fd064662cbfb0e542bee8c9898", nil
+		return "medyagh/kic:v1.16.1@sha256:6bbf560b7e9a7acb241e5f2e7c063ed6c9c87387f4b1657da73d98349c315049", nil
 	case "v1.16.2":
-		return "medyagh/kic:v1.16.2@sha256:2335bde84ead2ff9543c6729aa5edf16f77cc66c9e4feff69420eea697399391", nil
+		return "medyagh/kic:v1.16.2@sha256:9880a59ee94573cf257a5118ffc8dd4f3827229fdb41c59d6b6f785edc215f54", nil
 	default:
 		return "v1.15.0", fmt.Errorf("not supported version, using default version")
 	}
